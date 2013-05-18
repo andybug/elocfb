7f825f288c39784a2b2943f3ac3a67d7a49e77e7
Andrew Fields <andybug10@gmail.com>
2013-05-18 12:12:44 -0500

    Basic team structure and hashing functions

---
+++ src/teams.c
@@ -0,0 +1,54 @@
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "teams.h"
+
+static const int mask = MAX_TEAMS - 1;
+
+struct team teams[MAX_TEAMS];
+int num_teams = 0;
+
+void init_teams(void)
+{
+	memset(teams, 0, sizeof(struct team) * MAX_TEAMS);
+	num_teams = 0;
+}
+
+struct team *create_team(int key)
+{
+	int i = key;
+	int iters = 0;
+
+	while (teams[i & mask].key != 0) {
+		if (iters >= num_teams) {
+			fprintf(stderr, "%s: Team hash table full; could not add key '%d'\n", __func__, key);
+			exit(EXIT_FAILURE);
+		}
+
+		i++;
+		iters++;
+	}
+
+	teams[i].key = key;
+	return (teams + i);
+}
+
+struct team *find_team(int key)
+{
+	int i = key;
+	int iters = 0;
+
+	while (teams[i & mask].key != key) {
+		if (iters >= num_teams) {
+			fprintf(stderr, "%s: Could not find team with key '%d'\n", __func__, key);
+			exit(EXIT_FAILURE);
+		}
+
+		i++;
+		iters++;
+	}
+
+	return (teams + i);
+}
