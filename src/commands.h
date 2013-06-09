c2032cbb385e42a22cc86a8ef54c14ec80e0df04
Andrew Fields <andybug10@gmail.com>
2013-06-09 09:43:06 -0500

    Removed commands

--- src/commands.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMMANDS_H
-#define COMMANDS_H
-
-extern int cmd_init(int argc, char **argv);
-extern int cmd_parse(int argc, char **argv);
-extern int cmd_process(int argc, char **argv);
-extern int cmd_algo(int argc, char **argv);
-
-#endif
